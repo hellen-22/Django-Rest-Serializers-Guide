df5297b68f4328a58f2b1ddef44cfcef9f239058
hellen-22 <hellenwain@gmail.com>
2024-10-09 12:50:56 +0300

    add .env

--- rest_serializer/settings.py
+++ rest_serializer/settings.py
@@ -11,19 +11,22 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(str(BASE_DIR / ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qjt#(hq7(s53me&2hl*-0=*ol@ad#1b0vw1qhqm5y2-qd8@r-x"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if env("DEBUG") == "True" else False
 
 ALLOWED_HOSTS = []
 
